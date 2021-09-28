8068b54cbcfb548a4b51ef8373e431beac94fbfc
sagarKumawatOctal <octal#321>
2021-09-28 10:06:37 +0530

    Initial commit

---
+++ app/src/main/java/com/octal/actorpay/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.octal.actorpay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/octal/actorpay/SplashScreen.kt
@@ -0,0 +1,16 @@
+package com.octal.actorpay
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed(Runnable {
+           startActivity(Intent(this@SplashScreen,MainActivity::class.java))
+        },2000)
+    }
+}
