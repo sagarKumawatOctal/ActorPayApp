ca1590b3cdb887935cac856dbad860de685d398c
Sohans-1997 <sohan.singh@octalinfosolution.com>
2021-09-29 11:45:18 +0530

    login my oder wallet

---
+++ app/src/main/java/com/octal/actorpay/auth/SignUpScreenFragment.kt
@@ -0,0 +1,49 @@
+package com.octal.actorpay.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import com.octal.actorpay.R
+import com.octal.actorpay.databinding.FragmentLoginBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SignUpScreenFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/octal/actorpay/auth/ViewPagerAdapter.kt
@@ -0,0 +1,28 @@
+package com.octal.actorpay.auth
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class ViewPagerAdapter internal constructor(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    private val COUNT = 2
+    val tab = arrayOf("Login", "Signup")
+    override fun getItem(position: Int): Fragment {
+        var fragment: Fragment? = null
+        when (position) {
+            0 -> fragment = LoginScreenFragmentFragment()
+            1 -> fragment = SignUpScreenFragment()
+        }
+
+        return fragment!!
+    }
+
+    override fun getCount(): Int {
+        return COUNT
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return tab[position]
+    }
+}
